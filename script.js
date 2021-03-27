5fa22d141ac7cc5ad1c27871a5c98e5f610009b1
Stephen Yacas <syacas123@gmail.com>
2021-03-27 16:00:01 -0400

    edits to js and html

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let timeDisplayEl = $("#time-display");
 let saveBtn = document.getElementById("btn");
+let userTextInput = document.querySelector("#inputField");
 
-function displayTime() {
+
+//this function displays the current time and date in the header of the application
+function displayTime() { 
   var rightNow = moment().format("MMMM DD, YYYY [|] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
 setInterval(displayTime, 1000);
+renderMessege();
 
-
-
-
-
+//color corresponding to current time
 $(".time-block").each(function() {
 
   var hour = parseInt($(this).data().number);
@@ -18,40 +19,36 @@ $(".time-block").each(function() {
 
     if(currentTime > hour){
       $(this).addClass("past")
-      console.log("seth, whats up?");
-    }else if (currentTime < hour) {
+      // console.log("seth, whats up?");
+    } else if (currentTime < hour) {
       $(this).addClass("future");
     } else {
       $(this).addClass("present");
     }
-    console.log("are you working?", (this))
+    // console.log("are you working?", (this))
 });
 
-
-
-
-function writeNotes() {
-  console.log('Please');
-  var inputField = localStorage.getItem("inputField");
-  var hello = $(hello);
-  if (!inputField) {
-    return;
-  }
-hello.textContent = inputField.value;
+//testing below
+function renderMessege() { //this function allows for saved events to remain, even after the page has been refreshed
+  console.log("testing")
+  var userInput = localStorage.getItem("inputField")
+  
+  userTextInput.textContent = userInput;
+  
 }
-
-// function renderMessage() {
-//   input = JSON.parse(localStorage.getItem("input"));
-//   if (!localStorage.getItem("input")) {
-//     $window.localStorage.setItem("input", JSON.stringify($scope.initData));
-//     var inputDescription = document.querySelector("description");
-//     inputDescription.textContent = input;
-//   }
-// }
+//testing above
+//testing local storage part 2 below
 
 
-$(".saveBtn").on("click", function () {
+
+$(".saveBtn").on("click", function (event) {
   console.log($(this).siblings(".description")[0].value);
+  event.preventDefault();
+  // $("#hour-9 .description").val(localStorage.setItem("hour-9"));
+  // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
 });
 
 saveBtn.addEventListener("click", function (event) {
@@ -66,14 +63,18 @@ saveBtn.addEventListener("click", function (event) {
     console.log("success");
 
     localStorage.setItem("inputField", inputField);
+   
   }
 });
 
-// function renderInputs(e) {
 
-//   var inputField = localStorage.getElementById("inputField");
-//   if (!inputField) {
-//     return;
-//   }
-//   inputField.textContent = inputField;
+
+// for(i=9; i<18; i++) {
+//   $("hour-"+i+" .description").val(localStorage.getItem("hour-"+i));
 // }
+
+// $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+// $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+// $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+// $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+// $("#hour-13 .description").val(localStorage.getItem("hour-13"));
